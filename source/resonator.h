dad8b92bd03b9deb0e67b7ce1e1db35ffce12278
Greg Surges surgesg <Greg Surges surgesg@gmail.com>
2012-01-17 13:55:16 -0800

    reverted

--- source/resonator.h
+++ source/resonator.h
@@ -9,18 +9,15 @@
 
 class Resonator {
 public:
-	Resonator(float freq, float Q);
+	Resonator(int del_time, float decay_coef);
 	Resonator();
-	void set_params(float freq, float Q);
-	void set_sr(int SR);
-	void set_q(float Q);
+	void set_params(int del_time, float decay_coef);
 	float process(float input_sample);
-	float Q, cf;
+	
 private:
-	float sr;
+	float * delay_line;
 	float a0, a1, a2, b0, b1, b2;
-	float w0;
-	float cosW0, sinW0;
-	float alpha;
-	float x0, x1, x2, y1, y2;
+	int delay_time;
+	float decay_time;
+	int read_ptr, write_ptr;
 };
